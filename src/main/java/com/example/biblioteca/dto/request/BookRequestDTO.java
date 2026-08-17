bf6684198889f04cac76e90ad6c6d5330a44d76c
matheus-pelissari <matheus.p.albert@gmail.com>
2026-08-17 09:17:26 -0300

    feat: Entidade User

--- src/main/java/com/example/biblioteca/dto/request/BookRequestDTO.java
+++ src/main/java/com/example/biblioteca/dto/request/BookRequestDTO.java
@@ -1,4 +1,4 @@
-package dto.request;
+package com.example.biblioteca.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
--- src/main/java/com/example/biblioteca/dto/response/BookResponseDTO.java
+++ src/main/java/com/example/biblioteca/dto/response/BookResponseDTO.java
@@ -1,4 +1,22 @@
-package dto.response;
+package com.example.biblioteca.dto.response;
+
+import com.example.biblioteca.enums.BookStatus;
+
+import java.time.LocalDateTime;
 
 public class BookResponseDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private String publisher;
+    private Integer publicationYear;
+    private Integer totalCopies;
+    private Integer availableCopies;
+    private String category;
+    private BookStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
