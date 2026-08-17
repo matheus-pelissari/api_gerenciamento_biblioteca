71c9699a0820dde659b6c4a4151e84b0298dad02
matheus-pelissari <matheus.p.albert@gmail.com>
2026-08-17 10:44:45 -0300

    feat: LoanResponseDTO

--- src/main/java/com/example/biblioteca/dto/request/LoanRequestDTO.java
+++ src/main/java/com/example/biblioteca/dto/request/LoanRequestDTO.java
@@ -1,4 +1,30 @@
 package com.example.biblioteca.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoanRequestDTO {
+
+    @NotBlank(message = "O id do usuário é obrigatório")
+    private Long userId;
+
+    @NotBlank(message = "O id do livro é obrigatório")
+    private Long bookId;
+
+    public LoanRequestDTO(){}
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
 }
